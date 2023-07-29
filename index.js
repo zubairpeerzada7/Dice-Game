df5c026e322530cd0727601764f15166455148e7
Peerzada Zubair Ul Islam <zubairpeerzada07@gmail.com>
2023-07-29 13:17:14 +0530

    changes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-
 var randomDiceImage = "dice" + randomNumber1 + ".png";
-var randonImageSorce = "images/" + randomDiceImage;
+var randonImageSorce = "./images/" + randomDiceImage;
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randonImageSorce);
 
